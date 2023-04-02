76760049c17b3d4b983b16b00df86c5658d8eb43
miwpayou <miwpayou0808@gmail.com>
2023-04-02 15:28:55 +0900

    feat: Elasticache Redisリソース
    cdkも更新

--- lib/miwkey-public-stack.ts
+++ lib/miwkey-public-stack.ts
@@ -1,4 +1,4 @@
-import {Duration, RemovalPolicy, Stack} from 'aws-cdk-lib';
+import {aws_elasticache, Duration, RemovalPolicy, Stack} from 'aws-cdk-lib';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -6,6 +6,7 @@ import {Construct} from 'constructs';
 import {MiwkeyPublicStackProps} from "./types/stackprops";
 import {FileSystem} from "aws-cdk-lib/aws-efs";
 import {configFSPolicy} from "./iam-policies";
+import {CfnCacheCluster} from "aws-cdk-lib/aws-elasticache";
 
 export class MiwkeyPublicStack extends Stack {
     constructor(scope: Construct, id: string, props: MiwkeyPublicStackProps) {
@@ -29,5 +30,24 @@ export class MiwkeyPublicStack extends Stack {
             securityGroup: props.defaultSG,
             removalPolicy: RemovalPolicy.DESTROY
         })
+
+        const queueSubnetGroup = new aws_elasticache.CfnSubnetGroup(this, "miwkeyQueueNetwork", {
+            description: "subnet group for miwkey Redis",
+            subnetIds: props.mainSubnets.map(s => s.subnetId),
+            cacheSubnetGroupName: "miwkey-queue-network"
+        })
+        const postQueue = new CfnCacheCluster(this, "miwkeyQueueRedis", {
+            cacheNodeType: "cache.t4g.micro",
+            engine: "redis",
+            numCacheNodes: 1,
+            autoMinorVersionUpgrade: true,
+            azMode: "single-az",
+            cacheSubnetGroupName: queueSubnetGroup.cacheSubnetGroupName,
+            engineVersion: "6.2",
+            preferredMaintenanceWindow: "tue:19:00-tue:22:00", // 04:00JST-07:00JST
+            snapshotRetentionLimit: 5,
+            vpcSecurityGroupIds: [props.defaultSG.securityGroupId]
+        })
+        postQueue.node.addDependency(queueSubnetGroup)
     }
 }
