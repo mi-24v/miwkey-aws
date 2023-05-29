9383110f39c95f8c152ab05f5a8568ec946ee85e
miwpayou <miwpayou0808@gmail.com>
2023-05-30 00:48:53 +0900

    fix(ecs): RDS CA bundleを指定
    TLS必須なのにmisskey本体が接続失敗するため

--- lib/taskdef.ts
+++ lib/taskdef.ts
@@ -5,13 +5,21 @@ import {RetentionDays} from "aws-cdk-lib/aws-logs";
 
 const imageTag = "4d04a48b-arm64"
 const imageRepoUrl = "docker.io/miwpayou0808/misskey"
+const environmentVariables = {
+    NODE_EXTRA_CA_CERTS: "/misskey/.config/ap-northeast-1-bundle.pem"
+}
 
 export function miwkeyMainTaskDefinition(): ApplicationLoadBalancedTaskImageOptions {
     return {
         image: ContainerImage.fromRegistry(`${imageRepoUrl}:${imageTag}`),
         command: ["npm", "run", "start"],
         containerPort: 8080,
-        enableLogging: true
+        enableLogging: true,
+        environment: environmentVariables,
+        logDriver: LogDriver.awsLogs({
+            streamPrefix: "miwkey",
+            logRetention: RetentionDays.SIX_MONTHS
+        })
     }
 }
 
@@ -19,6 +27,7 @@ export function miwkeyMigrationTaskDefinition(): ContainerDefinitionOptions {
     return {
         image: ContainerImage.fromRegistry(`${imageRepoUrl}:${imageTag}`),
         command: ["npm", "run", "migrate"],
+        environment: environmentVariables,
         essential: false,
         logging: LogDriver.awsLogs({
             streamPrefix: "miwkey",
