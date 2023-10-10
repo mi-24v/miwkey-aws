474bc08ae0b919207def4620add64b59ebb35401
miwpayou <miwpayou0808@gmail.com>
2023-10-11 02:45:23 +0900

    feat(meilisearch): backup用のS3バケット作成

--- lib/meilisearch.ts
+++ lib/meilisearch.ts
@@ -11,9 +11,17 @@ import {
     SubnetSelection,
     Vpc
 } from "aws-cdk-lib/aws-ec2";
+import {Duration, RemovalPolicy} from "aws-cdk-lib";
+import {BlockPublicAccess, Bucket, BucketEncryption, StorageClass} from "aws-cdk-lib/aws-s3";
+import {IGrantable, ManagedPolicy, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
 
 
 export function meilisearchInstance(scope: Construct, vpc: Vpc, subnets: SubnetSelection, securityGroup: SecurityGroup) {
+    const instanceRole = new Role(scope, "miwkey-meiliseach-instance-role", {
+        assumedBy: new ServicePrincipal("ec2.amazonaws.com"),
+        managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")]
+    })
+    const bucket = meilisearchBackupStorage(scope, instanceRole)
     const instance = new Instance(scope, "miwkey-meilisearch", {
         instanceType: InstanceType.of(InstanceClass.T3A, InstanceSize.MICRO),
         machineImage: MachineImage.lookup({
@@ -32,9 +40,35 @@ export function meilisearchInstance(scope: Construct, vpc: Vpc, subnets: SubnetS
             }
         ],
         detailedMonitoring: true,
+        role: instanceRole,
         securityGroup: securityGroup,
         ssmSessionPermissions: true,
         vpcSubnets: subnets
     })
+    instance.node.addDependency(bucket)
     return (instance)
+}
+
+function meilisearchBackupStorage(scope: Construct, grantTarget: IGrantable) {
+    const bucket = new Bucket(scope, "meilisearch-backup-s3", {
+        blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+        encryption: BucketEncryption.S3_MANAGED,
+        enforceSSL: true,
+        lifecycleRules: [
+            {
+                enabled: true,
+                expiration: Duration.days(180),
+                prefix: "autoSnapshots/",
+                transitions: [
+                    {
+                        storageClass: StorageClass.GLACIER,
+                        transitionAfter: Duration.days(30)
+                    }
+                ]
+            }
+        ],
+        removalPolicy: RemovalPolicy.RETAIN
+    })
+    bucket.grantReadWrite(grantTarget)
+    return bucket
 }
