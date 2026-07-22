676984c733b5b321ad75fb5810bfd5e70afe33d4
miwpayou <miwpayou0808@gmail.com>
2026-07-22 13:06:00 +0900

    test(stack): repair public stack fixture

--- test/miwkey-public.test.ts
+++ test/miwkey-public.test.ts
@@ -1,14 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
-import { Template, Match } from 'aws-cdk-lib/assertions';
+import { Match, Template } from 'aws-cdk-lib/assertions';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { SecurityGroup, Subnet, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import * as MiwkeyPublic from '../lib/miwkey-public-stack';
 
-test('SQS Queue and SNS Topic Created', () => {
+function createTemplate(): Template {
   const app = new cdk.App();
-  // WHEN
-  const stack = new MiwkeyPublic.MiwkeyPublicStack(app, 'MyTestStack');
-  // THEN
+  const supportStack = new cdk.Stack(app, 'TestSupportStack');
+  const vpc = new Vpc(supportStack, 'TestVpc', {
+    maxAzs: 2,
+    natGateways: 0,
+    subnetConfiguration: [{ name: 'test-public', subnetType: SubnetType.PUBLIC }]
+  });
+  const stack = new MiwkeyPublic.MiwkeyPublicStack(app, 'MyTestStack', {
+    mainVpc: vpc,
+    mainSubnets: vpc.publicSubnets.map(subnet => subnet as Subnet),
+    defaultSG: new SecurityGroup(supportStack, 'TestDefaultSG', { vpc }),
+    loadBalancerSG: new SecurityGroup(supportStack, 'TestLoadBalancerSG', { vpc }),
+    domainCertificate: Certificate.fromCertificateArn(
+      supportStack,
+      'TestCert',
+      'arn:aws:acm:ap-northeast-1:123456789012:certificate/00000000-0000-0000-0000-000000000000'
+    )
+  });
 
-  const template = Template.fromStack(stack);
+  return Template.fromStack(stack);
+}
+
+test('SQS Queue and SNS Topic Created', () => {
+  const template = createTemplate();
 
   template.hasResourceProperties('AWS::SQS::Queue', {
     VisibilityTimeout: 300
